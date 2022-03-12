e18f16733016a8a8de20906dc6368b321d3885e3
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-03-11 21:19:53 -0500

    Initial dependencies configuration

---
+++ src/test/java/spring/security/user/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.user.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
